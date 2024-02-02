221703ea5e0a15116d04bbd7c34039bdd9c84533
SilvanoValdivia <silvano.valdivia9295@alumnos.udg.mx>
2024-02-01 19:14:45 -0600

    Create Impresordedatos.cpp

---
+++ Impresordedatos.cpp
@@ -0,0 +1,22 @@
+/**************************Presentación*****************************************
+Nombre:Silvano Joset Valdivia Franco
+Fecha: 29 de Enero del 2024
+Programa: impresor de datos
+Centro Universitario de los Altos/Universidad de Guadalajara
+Ingeniería en computacion/2do semestre
+Profesor: Sergio Franco Casillas
+Descripción:El siguente programa tiene el objetivo de imprimir una serie de
+datos especificos de una manera ordenada.
+*******************************************************************************/
+# include <iostream>
+# include <stdio.h>
+using namespace std;
+int main (){
+printf("Nombre:Silvano Joset Valdivia Franco "); /*imprime el nombre*/
+printf("Edad:19 años "); /*imprime la edad*/
+printf("Lugar de residencia: Tepatitlán de Morelos jalisco "); /*imprime la residencia*/
+printf("Fecha:29 de enero del 2024 "); /*imprime la fecha*/
+printf("Carrera:Ingeniería en computacion "); /*imprime la carrera*/
+printf("Semestre:2do Semestre "); /*imprime el semestre*/
+    return 0;
+}
