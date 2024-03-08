78dc1b56fbe1dbb0c90b24eed6c39d55c0b71c2b
SilvanoValdivia <silvano.valdivia9295@alumnos.udg.mx>
2024-03-07 21:46:36 -0600

    Create mylib.cpp

---
+++ mylib.cpp
@@ -0,0 +1,27 @@
+void arreglo (){
+if (pos=10) {
+cout<< "Arreglo lleno"<<endl;
+} else {
+ar [pos]=val1; //ingresar el valor en la posicion
+pos++ //incremento de la posicion
+} // fin else
+}
+
+void vector (){
+vec [pos]=val1; //ingresar el valor en la posicion
+pos++; //incremento de la posicion
+break;
+}
+
+void printarreglo (){
+for (int i=0; 1 1<pos; i++){
+cout<< "{" ar[i]<< "}, " endl;
+} //fin for
+}
+
+void printvector (){
+for (int i=0; 1; 1<pos; i++){
+cout<< "{" vec[i]<< "}, " endl;
+} //fin for
+}
+
