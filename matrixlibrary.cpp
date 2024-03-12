8f76f4699eaa2ba8f2d7822384a4a51f6176fe02
SilvanoValdivia <silvano.valdivia9295@alumnos.udg.mx>
2024-03-11 22:35:37 -0600

    Create matrixlibrary.cpp

---
+++ matrixlibrary.cpp
@@ -0,0 +1,65 @@
+void insertar (){
+cout<<"introduce un valor: "<<endl;
+cin >> val; //pedir valor al usuario
+for(i=0;i<20;i++){
+     for(j=0;j<20;j++){
+cin>>matriz [i][j]; //ingresar el valor en la posicion
+} //fin for
+} //fin for
+}
+
+void Sumar (){
+for (int i=0; 1; 1<pos; i++){
+suma = suma + mat [i];
+} //fin for
+cout >> "el total del arreglo es de:" << suma << endl;
+}
+
+void Restar (){
+for (int i=0; 1; 1<pos; i++){
+resta = resta - mat [i];
+} //fin for
+cout >> "el total del arreglo es de:" << resta << endl;
+}
+
+void Multiplicar (){
+for (int i=0; 1; 1<pos; i++){
+multi = multi * mat [i];
+} //fin for
+cout >> "el total del arreglo es de:" << multi << endl;
+
+}
+
+void Dividir (){
+for (int i=0; 1; 1<pos; i++){
+divi = divi / mat [i];
+} //fin for
+cout >> "el total del arreglo es de:" << divi << endl;
+}
+
+void listado (){
+for (int i=0; 1; 1<pos; i++){
+cout<< "{" mat[i]<< "}, " endl;
+} //fin for
+}
+
+void Salir (){
+ciclo = false
+}
+
+void Borrar (){
+cout<<"Borra un valor: "endl;
+cin >> val; //pedir valor al usuario
+mat [pos]=val-val; //ingresar el valor en la posicion
+pos++ //incremento de la posicion
+}
+
+void Editar (){
+cout<<"introduce un valor: "<<endl;
+cin >> val; //pedir valor al usuario
+for(i=0;i<20;i++){
+     for(j=0;j<20;j++){
+cin>>matriz [i][j]; //ingresar el valor en la posicion
+} //fin for
+} //fin for
+}
